c72ee75efaf4ff49f8dfab9aac79fc3dfa21d4e1
karinamelo26 <karinamelo26@gmail.com>
2022-07-27 22:17:23 -0300

    Configured Material UI

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import '@styles/index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <CssBaseline />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
